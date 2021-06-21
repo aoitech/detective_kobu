bb707e29528e29486d42dd98d6b03199df628083
Aoi Onishi <53684526+aoitech@users.noreply.github.com>
2021-06-22 08:33:12 +0900

    画像の表示

--- rpg/javascript/engine/game.js
+++ rpg/javascript/engine/game.js
@@ -6,11 +6,16 @@
 // ゲーム作りの基本となるクラス
 class Game {
   // 引数width,heightはゲームの横・縦幅
+  // constructorはclassが使われるときに実行される。初期化するためのメソッド。
+  // constructorでcanvasを使うための準備をする。
   constructor(width, height) {
     this.canvas = document.createElement('canvas');
     document.body.appendChild(this.canvas);
     this.canvas.width = width || 320;
     this.canvas.height = height || 320;
+
+    //ゲームに登場する全てのもの（オブジェクト）を入れるための配列
+    this.objs = [];
   }
 
 // startメソッドを呼び出してメインループ開始
@@ -29,8 +34,23 @@ _mainLoop() {
   //塗りつぶしの四角形を描く際に使用。左上から、画面のサイズまでを、塗りつぶす。
   ctx.fillRect( 0,0,this.canvas.width,this.canvas.height );
 
+  //ゲームに登場する全てのもの（オブジェクト）の数だけ繰り返す
+  // 第一引数はカウンター変数を初期化するため、第二引数はループの処理前に評価され、trueならfor内が実行される
+  // 第三引数はループ後に変数を1ずつ増加
+  for ( let i = 0; i<this.objs.length; i++) {
+    //スプライトやテキストなど、すべてのオブジェクトのupdateメソッドを呼び出す
+    // this.objs[i].updateでsprite.jsのSpriteクラスのupdateメソッドを呼び出している
+    this.objs[i].update(this.canvas);
+  }
+
   // bindは引数にthisを指定して振る舞いを変える事ができる
   requestAnimationFrame(this._mainLoop.bind(this));
+}
+
+// オブジェクトをゲームに追加できるようになる、addメソッドを作成
+// 引数obj : 追加したいオブジェクト
+add(obj) {
+  this.objs.push(obj);
 }
 
 }
