4629b8a4ee5fde54b049e8cd96cbb981d90dacf0
Aoi Onishi <53684526+aoitech@users.noreply.github.com>
2021-06-22 08:55:34 +0900

    何番目の画像かを指定

--- rpg/javascript/engine/sprite.js
+++ rpg/javascript/engine/sprite.js
@@ -6,7 +6,7 @@ class Sprite {
 	//  * img : 画像ファイルまでのパス
 	//  * width : 画像の表示する範囲（横幅）
 	//  * height : 画像の表示する範囲（縦幅）
-  constructor(img) {
+  constructor(img,width,height) {
     //this.imgに、あなたは画像ですよ、と教える
     //new Imageで新たなimg要素を作成
     this.img = new Image();
@@ -14,6 +14,16 @@ class Sprite {
     this.img.src = img;
     //画像の初期位置
     this.x = this.y = 0;
+
+
+    //画像表示の幅を指定
+    //画像を表示する範囲の横幅。引数widthが指定されていない場合、this.widthに32を代入
+		this.width = width || 32;
+		//画像を表示する範囲の縦幅。引数heightが指定されていない場合、this.heightに32を代入
+		this.height = height || 32;
+		//12個の画像があった場合に何番目の画像を表示するか。
+    //frameはスプライトに設定されているフレームを示す
+		this.frame = 0;
   }
 
 
@@ -27,10 +37,29 @@ class Sprite {
   }
 
   render(canvas) {
+    //キャンバスの外にスプライトがあるとき、ここでこのメソッドを終了する
+    if (this.x < -1 * this.width || this.x > canvas.width) return;
+    if (this.y < -1 * this.height || this.y > canvas.height) return;
+
+    //X,Y方向に、何番目の画像か
+    const _frameX = this.frame % (this.img.width /this.width);
+    // ~~は小数点切り捨ての意味。
+    const _frameY = ~~(this.frame / (this.img.width /this.width ) );
+    
     //画家さん（コンテキスト）を呼ぶ
     const _ctx = canvas.getContext('2d');
     //画家さんに、絵を描いてとお願いする
     // drawImageは使用範囲を指定してイメージを描画する
-    _ctx.drawImage(this.img,this.x,this.y);
+    _ctx.drawImage(
+      this.img,
+			this.width * _frameX,
+			this.height * _frameY,
+			this.width,
+			this.height,
+			this.x,
+			this.y,
+			this.width,
+			this.height
+      );
   }
 }
