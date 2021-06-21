3f11be4ea89cf5a2295d3332acb30ca9f4199216
Aoi Onishi <53684526+aoitech@users.noreply.github.com>
2021-06-21 10:58:47 +0900

    create_GameClass

--- rpg/javascript/engine/game.js
+++ rpg/javascript/engine/game.js
@@ -1,2 +1,14 @@
-// ゲームエンジン
-console.log(222)
+// ゲームエンジン作成用ファイル
+
+'use strict'
+
+// ゲーム作りの基本となるクラス
+class Game {
+  // 引数width,heightはゲームの横・縦幅
+  constructor(width, height) {
+    this.canvas = document.createElement('canvas');
+    document.body.appendChild(this.canvas);
+    this.canvas.width = width || 320;
+    this.canvas.height = height || 320;
+  }
+}
