290e3baebca969b74ae4553842f45547ae8030ab
Aoi Onishi <53684526+aoitech@users.noreply.github.com>
2021-06-21 11:24:57 +0900

    create_sprite.js

--- rpg/javascript/engine/game.js
+++ rpg/javascript/engine/game.js
@@ -1,5 +1,6 @@
 // ゲームエンジン作成用ファイル
 
+// より厳格なエラーチェックを行う宣言
 'use strict'
 
 // ゲーム作りの基本となるクラス
@@ -11,4 +12,24 @@ class Game {
     this.canvas.width = width || 320;
     this.canvas.height = height || 320;
   }
+
+// startメソッドを呼び出してメインループ開始
+start() {
+  this._mainLoop();
+}
+
+// _から始まるのはprivateの意。
+_mainLoop() {
+  // getContextはキャンパスに描画するためのオブジェクト(コンテキスト)を取得するメソッド
+  // 2dは2次元グラフィックのコンテキストを取得
+  const ctx = this.canvas.getContext('2d')
+  // fillStyleはcanvasで図形の内側を塗りつぶすために色などを指定できる
+  ctx.fillStyle = '#000000';
+  //塗りつぶしの四角形を描く際に使用。左上から、画面のサイズまでを、塗りつぶす。
+  ctx.fillRect( 0,0,this.canvas.width,this.canvas.height );
+
+  // bindは引数にthisを指定して振る舞いを変える事ができる
+  requestAnimationFrame(this._mainLoop.bind(this));
+}
+
 }
--- rpg/javascript/main.js
+++ rpg/javascript/main.js
@@ -1,1 +1,10 @@
-console.log(111)
+// より厳格なエラーチェックを行う宣言
+'use strict'
+
+addEventListener('load',() => {
+  //定数gameに、あなたはゲームですよ、と教える
+  const game = new Game();
+
+  //gameに、ゲームをスタートして、とお願いする
+	game.start(1136, 640);
+})
