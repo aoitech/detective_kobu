1c2db5542408ed59907e2d94bb826443aa0af7a7
Aoi Onishi <53684526+aoitech@users.noreply.github.com>
2021-06-22 15:46:41 +0900

    add_onenterframe_scene.js

--- rpg/javascript/engine/scene.js
+++ rpg/javascript/engine/scene.js
@@ -9,4 +9,17 @@ class Scene {
   add(obj) {
     this.objs.push(obj);
   }
+
+  // Gameクラスのメインループからずっと呼び出され続ける
+  // 引数
+	//  * canvas : 紙（キャンバス）
+  
+  // ここで23行目のonenterframeメソッドを呼び出す
+  update(canvas) {
+    //スプライトを動かしたり、なにかのきっかけでイベントを発生させたりするために使うメソッドを呼び出す
+    this.onenterframe();
+  }
+  
+  // 常に呼び出され、スプライトの移動やイベントの発生などに使うメソッド。空なのはオーバーライドして使うため
+  onenterframe() {}
 }
