e25c13c4c6e4c9580454a59d1933aeac5766bdb5
Aoi Onishi <53684526+aoitech@users.noreply.github.com>
2021-06-22 14:07:50 +0900

    add_animation

--- rpg/javascript/main.js
+++ rpg/javascript/main.js
@@ -5,8 +5,18 @@ addEventListener('load',() => {
   //定数gameに、あなたはゲームですよ、と教える
   const game = new Game();
 
+  const WALKING_SPEED = 4;
   //変数yamadaに、あなたは山田先生のスプライト画像ですよ、と教える
 	const yamada = new Sprite( 'img/yamada.png' );
+
+yamada.onenterframe = () => {
+  //キーが押されたとき、山田先生が移動する
+  if(game.input.left)yamada.x -= WALKING_SPEED;
+  if (game.input.right) yamada.x += WALKING_SPEED;
+  if (game.input.up) yamada.y -= WALKING_SPEED;
+  if (game.input.down) yamada.y += WALKING_SPEED;
+}
+
 	//gameに、山田先生のスプライト画像を表示して、とお願いする
 	game.add( yamada );
 
